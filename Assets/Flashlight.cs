711344020b690033445f88337510e64641a395e1
eby8zevin <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2020-04-23 07:27:13 +0700

    2018.4.17f1 (64-bit)

---
+++ Assets/Flashlight.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Flashlight : MonoBehaviour {
+
+	public AudioClip soundOn;
+	public AudioClip soundOff;
+
+	void Update () 
+	{
+		if(Input.GetKeyDown(KeyCode.Mouse1))
+		{
+			if(GetComponent<Light>().enabled == false)
+			{
+				GetComponent<Light>().enabled = true;
+				GetComponent<AudioSource>().clip = soundOn;
+				GetComponent<AudioSource>().Play();
+			}
+			else
+			{
+				GetComponent<Light>().enabled = false;
+				GetComponent<AudioSource>().clip = soundOff;
+				GetComponent<AudioSource>().Play();
+			}
+		}
+	}
+}
